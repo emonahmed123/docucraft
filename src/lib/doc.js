53d93ebd3e98d7aafdcde5296d51d485d7a809ef
Emon Ahmed <imonshomon@gmail.com>
2025-08-06 21:01:58 +0600

    read md file

--- src/lib/doc.js
+++ src/lib/doc.js
@@ -1,10 +1,29 @@
-
+import fs from "fs"
+import matter from "gray-matter"
 import path from "path"
+const postsDirectory = path.join(process.cwd(), "src/doc")
 
-
-const postsDirectory = path.join(process.cwd(), "docs")
-
-console.log(postsDirectory)
+// console.log(postsDirectory, "fuck")
 export function getDocuments() {
 
+    const fileNames = fs.readdirSync(postsDirectory)
+    // console.log(fileNames, "fileNames")
+    const allDocuments = fileNames.map((fileName) => {
+
+        const id = fileName.replace(".md", "   ")
+
+        const fullPath = path.join(postsDirectory, fileName)
+        const fileContents = fs.readFileSync(fullPath, "utf8")
+
+
+        const matterResult = matter(fileContents)
+
+
+        console.log(matterResult, "matterResult")
+
+        return {
+            id,
+            ...matterResult.data,
+        }
+    })
 }
