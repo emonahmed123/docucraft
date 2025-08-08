bcb694a4cb37d1440e7672298f9b6791f53df7eb
Emon Ahmed <imonshomon@gmail.com>
2025-08-08 23:22:40 +0600

    change search

--- src/lib/doc.js
+++ src/lib/doc.js
@@ -1,8 +1,9 @@
 import fs from "fs"
 import matter from "gray-matter"
 import path from "path"
+import { remark } from "remark"
+import html from "remark-html"
 const postsDirectory = path.join(process.cwd(), "src/doc")
-
 // console.log(postsDirectory, "fuck")
 export function getDocuments() {
 
@@ -29,12 +30,29 @@ export function getDocuments() {
 
 
     return allDocuments.sort((a, b) => {
-        if (a.order > b.order) {
+        if (a.order < b.order) {
             return -1
         }
-        if (a.order < b.order) {
+        if (a.order > b.order) {
             return 1
         }
         return 0
     })
+}
+
+export async function getDocumentContent(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark().use(html).process(matterResult.content);
+
+    const contentHtml = processedContent.toString();
+
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data,
+    }
 }
